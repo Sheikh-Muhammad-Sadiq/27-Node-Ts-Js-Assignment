39be0d567770d102ccea4aec6ff9bc6bb9c1408d
Sheikh-Muhammad-Sadiq <162907621+Sheikh-Muhammad-Sadiq@users.noreply.github.com>
2024-03-17 04:06:27 -0700

    Add files via upload
    Q 27 Node.js of 45 Assignments

---
+++ Q27.js
@@ -0,0 +1,36 @@
+"use strict";
+// 27. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+// • If the alien is green, print a message that the player earned 5 points.
+// • If the alien is yellow, print a message that the player earned 10 points.
+// • If the alien is red, print a message that the player earned 15 points.
+// • Write three versions of this program, making sure each message is printed for the appropriate color alien.
+// let aliencolor: string = "green"
+// if(aliencolor == "green"){
+//     console.log("Player just in earned 5 points!")
+// }
+// else if(aliencolor === "yellow"){
+//     console.log("Player just in earned 10 points!")
+// }
+// else if(aliencolor === "red"){
+//     console.log("Player just in earned 15 points!")
+// }
+// let aliencolor: string = "yellow"
+// if(aliencolor == "green"){
+//     console.log("Player just in earned 5 points!")
+// }
+// else if(aliencolor === "yellow"){
+//     console.log("Player just in earned 10 points!")
+// }
+// else if(aliencolor === "red"){
+//     console.log("Player just in earned 15 points!")
+// }
+let aliencolor = "red";
+if (aliencolor == "green") {
+    console.log("Player just in earned 5 points!");
+}
+else if (aliencolor === "yellow") {
+    console.log("Player just in earned 10 points!");
+}
+else if (aliencolor === "red") {
+    console.log("Player just in earned 15 points!");
+}
